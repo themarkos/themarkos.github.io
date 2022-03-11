b485e2dd68105b3f06f2d09dd5f2271352763388
Markos Weissinger <100616695+themarkos@users.noreply.github.com>
2022-03-11 18:55:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
