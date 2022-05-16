65bf727c55291c4616bf5c8f6a4300f19bbcb8d3
Markos Weissinger <100616695+themarkos@users.noreply.github.com>
2022-05-15 21:01:47 -0400

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-import './style.css'
-
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-
-import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
